7adc175f40c27a36920402e951f1d743f3f1a755
HanJae-Jae <gkswowo98@naver.com>
2024-10-28 00:18:07 +0900

    [feat] 단어 엔티티 마이그레이션

---
+++ src/main/kotlin/com/hunmin/domain/entity/WordScore.kt
@@ -0,0 +1,21 @@
+package com.hunmin.domain.entity
+
+import jakarta.persistence.*
+
+@Entity
+data class WordScore (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val wordScore: Long = 0,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    var member: Member? = null,
+
+    var testLang: String? = null,
+
+    var testScore: Int? = null,
+
+    var testRankScore: Double? = null
+
+) : BaseTimeEntity()
