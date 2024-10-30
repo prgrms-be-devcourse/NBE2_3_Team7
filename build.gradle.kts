db86b3d6b2efe774162ee07e5d1ffe9160961446
DongWooKim4343 <kd6904@naver.com>
2024-10-31 02:28:57 +0900

    [feat] NoticeRepository test 구현

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     // kotlin-logging 의존성
     implementation("io.github.microutils:kotlin-logging-jvm:2.0.11") 
-    implementation("group: 'ch.qos.logback', name: 'logback-classic', version: '1.2.6'")
+    //implementation("group: 'ch.qos.logback', name: 'logback-classic', version: '1.2.6'")
+
 
     // ModelMapper
     implementation("org.modelmapper:modelmapper:3.1.1")
