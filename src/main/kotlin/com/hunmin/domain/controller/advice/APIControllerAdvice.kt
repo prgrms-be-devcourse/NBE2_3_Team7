4f35d06f6cd022849525309ab24f4cb24d77170a
DongWooKim4343 <kd6904@naver.com>
2024-10-31 12:36:41 +0900

    [feat] 공지사항, 관리자 예외처리 컨트롤러

--- src/main/kotlin/com/hunmin/domain/controller/advice/APIControllerAdvice.kt
+++ src/main/kotlin/com/hunmin/domain/controller/advice/APIControllerAdvice.kt
@@ -1,8 +1,6 @@
 package com.hunmin.domain.controller.advice
 
-import com.hunmin.domain.exception.BoardTaskException
-import com.hunmin.domain.exception.CommentTaskException
-import com.hunmin.domain.exception.NotificationTaskException
+import com.hunmin.domain.exception.*
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -27,6 +25,20 @@ class APIControllerAdvice {
     // 알림 예외 처리
     @ExceptionHandler(NotificationTaskException::class)
     fun handleNotificationTaskException(e: NotificationTaskException): ResponseEntity<Map<String, String>> {
+        val map = mapOf("error" to e.message)
+        return ResponseEntity.status(e.code).body(map)
+    }
+
+    // 공지사항 예외 처리
+    @ExceptionHandler(NoticeTaskException::class)
+    fun handleNoticeTaskException(e: NoticeTaskException): ResponseEntity<Map<String, String>> {
+        val map = mapOf("error" to e.message)
+        return ResponseEntity.status(e.code).body(map)
+    }
+
+    // 관리자 예외 처리
+    @ExceptionHandler(AdminTaskException::class)
+    fun handleAdminTaskException(e: AdminTaskException): ResponseEntity<Map<String, String>> {
         val map = mapOf("error" to e.message)
         return ResponseEntity.status(e.code).body(map)
     }
