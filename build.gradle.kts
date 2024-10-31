694caafe8cfefde8c9359520abd26abfadb05902
DongWooKim4343 <kd6904@naver.com>
2024-10-31 16:41:47 +0900

    [del] 중복제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,9 +79,6 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
-    // kotlin-logging 의존성
-    implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
-    implementation("group: 'ch.qos.logback', name: 'logback-classic', version: '1.2.6'")
 }
 
 kotlin {
