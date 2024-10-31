8feec3dd71908a5000a2f039e585c2933f1ce003
kang-ye-jin <yejin331@gachon.ac.kr>
2024-11-01 01:13:01 +0900

    [fix] 역직렬화 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,9 +79,8 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
-    // kotlin-logging 의존성
-    implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
-    implementation("group: 'ch.qos.logback', name: 'logback-classic', version: '1.2.6'")
+    // LocalDateTime 역직렬화 해결
+    implementation ("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 }
 
 kotlin {
--- src/main/kotlin/com/hunmin/domain/config/ModuleConfig.kt
+++ src/main/kotlin/com/hunmin/domain/config/ModuleConfig.kt
@@ -3,6 +3,7 @@ package com.hunmin.domain.config
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -12,6 +13,7 @@ class ModuleConfig {
     @Bean
     fun objectMapper(): ObjectMapper {
         return ObjectMapper().apply {
+            registerModule(KotlinModule.Builder().build())
             registerModule(JavaTimeModule())
             disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
         }
