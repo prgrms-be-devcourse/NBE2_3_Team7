df67a3731a9cb6a9fb8cc30b396b146949eaf742
iam52 <tupac52@gmail.com>
2024-11-03 16:54:43 +0900

    [feat] aws s3 설정

---
+++ src/main/kotlin/com/hunmin/domain/config/s3Config.kt
@@ -0,0 +1,30 @@
+package com.hunmin.domain.config;
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3Client
+
+@Configuration
+class s3Config(
+    @Value("\${cloud.aws.credentials.access-key}")
+    private val accessKey: String,
+
+    @Value("\${cloud.aws.credentials.secret-key}")
+    private val secretKey: String,
+
+    @Value("\${cloud.aws.region.static}")
+    private val region: String
+) {
+    @Bean
+    fun s3Client(): S3Client {
+        val awsCredentials = AwsBasicCredentials.create(accessKey, secretKey)
+
+        return S3Client.builder()
+            .region(Region.of(region))
+            .credentialsProvider { awsCredentials }
+            .build()
+    }
+}
