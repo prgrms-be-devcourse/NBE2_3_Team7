978e5d6cc3e1206787e83e9a2f6743278bae7c78
iam52 <tupac52@gmail.com>
2024-11-03 22:00:30 +0900

    [mod] S3FileManagement를 component 사용을 위해 @Component 설정,  HunminApplication에 컴포넌트 스캔 설정

--- src/main/kotlin/com/hunmin/domain/HunminApplication.kt
+++ src/main/kotlin/com/hunmin/domain/HunminApplication.kt
@@ -2,12 +2,14 @@ package com.hunmin.domain
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ComponentScan(basePackages = ["com.hunmin"])
 @EnableJpaRepositories(basePackages = ["com.hunmin.domain.repository"])
 @EnableRedisRepositories(
     basePackages = ["com.hunmin.domain.redis.repository"],
--- src/main/kotlin/com/hunmin/global/s3/S3FileManagement.kt
+++ src/main/kotlin/com/hunmin/global/s3/S3FileManagement.kt
@@ -4,13 +4,14 @@ import com.hunmin.global.exception.ExceptionCode
 import com.hunmin.global.validate.FileValidate
 import com.hunmin.global.validate.FileValidate.Companion.createUniqueFileName
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
 import org.springframework.web.multipart.MultipartFile
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
 
-
+@Component
 class S3FileManagement(
     @Value("\${cloud.aws.s3.bucket}")
     private val bucket: String,
