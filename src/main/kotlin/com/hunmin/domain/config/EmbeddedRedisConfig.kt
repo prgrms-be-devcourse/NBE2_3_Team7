ed8bf1a72deadf4ef08ce8c5ae5bddef4d6f4170
Dom1046 <rkrkwnj1046@gmail.com>
2024-11-03 12:11:54 +0900

    [refactor] 레디스 설정 리펙토링

--- src/main/kotlin/com/hunmin/domain/config/EmbeddedRedisConfig.kt
+++ src/main/kotlin/com/hunmin/domain/config/EmbeddedRedisConfig.kt
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import redis.embedded.RedisServer
+import java.io.IOException
+import java.net.Socket
 
-//로컬 환경일경우 내장 레디스가 실행된다.
+// 로컬 환경일 경우 내장 레디스가 실행된다.
 @Profile("local")
 @Configuration
 class EmbeddedRedisConfig(
@@ -19,14 +21,30 @@ class EmbeddedRedisConfig(
 
     @PostConstruct
     fun startRedisServer() {
-        redisServer = RedisServer(redisPort)
-        redisServer.start()
+        if (!isRedisRunning()) {
+            // Redis가 실행 중이 아니면 임베디드 Redis 서버 시작
+            redisServer = RedisServer(redisPort)
+            redisServer.start()
+            println("임베디드 Redis 서버가 시작되었습니다.")
+        } else {
+            println("Redis가 이미 실행 중입니다.")
+        }
     }
 
     @PreDestroy
     fun stopRedisServer() {
-        if (::redisServer.isInitialized) {
+        if (::redisServer.isInitialized && redisServer.isActive) {
             redisServer.stop()
+            println("임베디드 Redis 서버가 중지되었습니다.")
+        }
+    }
+
+    // Redis 서버가 실행 중인지 확인하는 메서드
+    private fun isRedisRunning(): Boolean {
+        return try {
+            Socket("localhost", redisPort).use { socket -> true }
+        } catch (e: IOException) {
+            false
         }
     }
 }
--- src/main/kotlin/com/hunmin/domain/redis/sendMessage/RedisSubscriber.kt
+++ src/main/kotlin/com/hunmin/domain/redis/sendMessage/RedisSubscriber.kt
@@ -20,6 +20,7 @@ class RedisSubscriber(
     //메세지를 구독자들에게 송신
     fun sendMessage(publishMessage: ChatMessageDTO) {
         try {
+            log.info("publishing message: $publishMessage")
             messagingTemplate.convertAndSend("/sub/chat/room/${publishMessage.chatRoomId}", publishMessage)
         } catch (e: Exception) {
             log.error("Exception {}", e)
@@ -33,6 +34,7 @@ class RedisSubscriber(
 
             val chatMessage = objectMapper.readValue(publishMessage, ChatMessageDTO::class.java)
 
+            log.info("publishMessage message: $publishMessage")
             messagingTemplate.convertAndSend("/sub/chat/room/${chatMessage.chatRoomId}", chatMessage)
         } catch (e: Exception) {
             log.error(e.message)
